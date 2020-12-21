99dbd082d7e3e5522f3e79e22d66b4eac3219d73
Jihogrammer <jiho@kakao.com>
2020-12-21 21:50:18 +0900

    egoing-web2-nodejs-함수의 기본 문법

---
+++ egoing-WEB2-Node.js/syntax/function.js
@@ -0,0 +1,22 @@
+function f123() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
+
+console.log('Pariatur cillum tempor excepteur nostrud magna.');
+console.log('Proident nulla sint elit labore non ad.');
+console.log('Lorem proident nostrud occaecat ut aute exercitation quis minim.');
+f123();
+console.log('Duis adipisicing velit ad consequat laboris sit irure et duis aliquip dolor nostrud.');
+console.log('Adipisicing do commodo non nulla eu laborum tempor pariatur aliquip pariatur.');
+console.log('Commodo aute reprehenderit do ad nulla amet Lorem aliqua ex.');
+f123();
+console.log('Proident exercitation officia pariatur deserunt officia ad nisi sint commodo do amet reprehenderit fugiat dolor.');
+console.log('Reprehenderit veniam sint labore anim eiusmod.');
+console.log('Elit aliqua labore velit nulla adipisicing cillum anim amet tempor Lorem.');
+f123();
+console.log('Labore fugiat deserunt elit nulla qui fugiat.');
+console.log('Cillum aliqua ea laborum id et aliquip voluptate tempor laboris ex ullamco quis.');
+console.log('Aliquip tempor cupidatat reprehenderit officia magna dolor.');
+f123();
