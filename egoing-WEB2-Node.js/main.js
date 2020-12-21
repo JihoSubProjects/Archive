6e9a2232c7f61bdf495c250d57372a4514e68e10
Jihogrammer <jiho@kakao.com>
2020-12-21 21:42:14 +0900

    egoing-web2-nodejs-글목록 출력하기

--- egoing-WEB2-Node.js/main.js
+++ egoing-WEB2-Node.js/main.js
@@ -10,9 +10,19 @@ var app = http.createServer(function(request, response) {
     // console.log(url.parse(_url, true));
     if (pathname == '/'/* root */) {
         if (queryData.id === undefined) {
-            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, descrption) {
+            fs.readdir('./data', function(err, filelist) {
+                // console.log(filelist);
                 var title = 'Welcome';
                 var descrption = 'Hello, Node.js';
+
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i = i + 1;
+                }
+                list = list + '</ul>';
+
                 var template = `
                 <!doctype html>
                 <html>
@@ -22,11 +32,7 @@ var app = http.createServer(function(request, response) {
                 </head>
                 <body>
                     <h1><a href="/">WEB</a></h1>
-                    <ol>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                    </ol>
+                    ${list}
                     <h2>${title}</h2>
                     <p>${descrption}</p>
                 </body>
@@ -37,30 +43,36 @@ var app = http.createServer(function(request, response) {
                 response.end(template);
             });
         } else {
-            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, descrption) {
-                var title = queryData.id;
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <ol>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                    </ol>
-                    <h2>${title}</h2>
-                    <p>${descrption}</p>
-                </body>
-                </html>
-                `;
-        
-                response.writeHead(200); // 파일은 성공적으로 찾음
-                response.end(template);
+            fs.readdir('./data', function(err, filelist) {
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i = i + 1;
+                }
+                list = list + '</ul>';
+
+                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, descrption) {
+                    var title = queryData.id;
+                    var template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        ${list}
+                        <h2>${title}</h2>
+                        <p>${descrption}</p>
+                    </body>
+                    </html>
+                    `;
+            
+                    response.writeHead(200); // 파일은 성공적으로 찾음
+                    response.end(template);
+                });
             });
         }
     } else {
