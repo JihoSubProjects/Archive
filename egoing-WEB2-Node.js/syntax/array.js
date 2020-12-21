a68bed366dc568198e1432726c20dcd50569efb4
Jihogrammer <jiho@kakao.com>
2020-12-21 21:16:51 +0900

    egoing-web2-nodejs-배열

---
+++ egoing-WEB2-Node.js/syntax/array.js
@@ -0,0 +1,11 @@
+// CRUD
+var arr = ['A','B','C','D']; // literal
+console.log(arr[1]); // 'B'
+console.log(arr[3]); // 'D'
+
+arr[2] = 3;
+console.log(arr);
+console.log(arr.length);
+
+arr.push('E');
+console.log(arr);
