662a012b316e59c62bef1b04a09381f315de13b8
Jihogrammer <jiho@kakao.com>
2020-12-21 21:04:25 +0900

    egoing-web2-nodejs-홈페이지 구현

--- egoing-WEB2-Node.js/main.js
+++ egoing-WEB2-Node.js/main.js
@@ -6,34 +6,63 @@ var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
 
     // console.log(url.parse(_url, true));
-    if (pathname == '/') {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, descrption) {
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ol>
-                <h2>${title}</h2>
-                <p>${descrption}</p>
-            </body>
-            </html>
-            `;
-    
-            response.writeHead(200); // 파일은 성공적으로 찾음
-            response.end(template);
-        });
+    if (pathname == '/'/* root */) {
+        if (queryData.id === undefined) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, descrption) {
+                var title = 'Welcome';
+                var descrption = 'Hello, Node.js';
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ol>
+                    <h2>${title}</h2>
+                    <p>${descrption}</p>
+                </body>
+                </html>
+                `;
+        
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, descrption) {
+                var title = queryData.id;
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ol>
+                    <h2>${title}</h2>
+                    <p>${descrption}</p>
+                </body>
+                </html>
+                `;
+        
+                response.writeHead(200); // 파일은 성공적으로 찾음
+                response.end(template);
+            });
+        }
     } else {
         response.writeHead(404); // 파일 찾기 실패함
         response.end('Not found');
