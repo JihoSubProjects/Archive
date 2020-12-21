931a24048da14f3abc5616bdde1d499cb6b82fd5
Jihogrammer <jiho@kakao.com>
2020-12-21 20:59:07 +0900

    egoing-web2-nodejs-Not found 구현

--- egoing-WEB2-Node.js/main.js
+++ egoing-WEB2-Node.js/main.js
@@ -1,50 +1,42 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var template;
 
 var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
 
-    // console.log('URL: ' + _url);
-    console.log(queryData.id);
-
-    if (_url == '/') {
-        /* url = '/index.html'; */
-        title = 'Welcome';
+    // console.log(url.parse(_url, true));
+    if (pathname == '/') {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, descrption) {
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ol>
+                <h2>${title}</h2>
+                <p>${descrption}</p>
+            </body>
+            </html>
+            `;
+    
+            response.writeHead(200); // 파일은 성공적으로 찾음
+            response.end(template);
+        });
+    } else {
+        response.writeHead(404); // 파일 찾기 실패함
+        response.end('Not found');
     }
-
-    if (_url == '/favicon.ico') {
-        return response.writeHead(404);
-    }
-
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, descrption) {
-        template = `
-        <!doctype html>
-        <html>
-        <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${descrption}</p>
-        </body>
-        </html>
-        `;
-    });
-
-    // console.log(__dirname + url);
-    // response.end(fs.readFileSync(__dirname + _url));
-    response.end(template);
 });
 app.listen(3000);
