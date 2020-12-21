9e21161da14c1682f043caf5a2ca370412835ec3
Jihogrammer <jiho@kakao.com>
2020-12-21 21:58:13 +0900

    egoing-web2-nodejs-함수의 출력

--- egoing-WEB2-Node.js/syntax/function2.js
+++ egoing-WEB2-Node.js/syntax/function2.js
@@ -1,8 +1,14 @@
 console.log(Math.round(1.6)); // 2
 console.log(Math.round(1.4)); // 1
 
-function sum(first, second) { // parameter
+/* function sum(first, second) { // parameter
     console.log(first + second);
 }
 
-sum(2, 4); // argument
+sum(2, 4); // argument */
+
+function sum(first, second) {
+    return first + second;
+}
+
+console.log(sum(2, 4));
