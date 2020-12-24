31197d1a2c3df9e53cf3542fd2b19761e2d343ab
Jihogrammer <jiho@kakao.com>
2020-12-24 19:45:25 +0900

    egoing-web2-nodejs-객체 반복

--- EGOING-WEB2-NODEJS/syntax/object.js
+++ EGOING-WEB2-NODEJS/syntax/object.js
@@ -2,6 +2,12 @@
 var members = ['egoing', 'k8805', 'foo'];
 console.log(members[1]); // k8805
 
+var i = 0;
+while (i < members.length) {
+    console.log('array loop', members[i]);
+    i = i + 1;
+}
+
 // 객체는 중괄호
 var roles = {
     'programmer' : 'egoing',
@@ -9,3 +15,8 @@ var roles = {
     'manager' : 'foo'
 }
 console.log(roles.designer); // k8805
+console.log(roles['designer']); // k8805
+
+for (var n in roles) {
+    console.log('object =>', n, 'value =>', roles[n]);
+}
