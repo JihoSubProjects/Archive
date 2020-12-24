d0d9c0bf16e67a3788d402078ba811d4cb38b1a7
Jihogrammer <jiho@kakao.com>
2020-12-24 19:40:35 +0900

    egoing-web2-nodejs-객체 형식

---
+++ EGOING-WEB2-NODEJS/syntax/object.js
@@ -0,0 +1,11 @@
+// 배열은 대괄호
+var members = ['egoing', 'k8805', 'foo'];
+console.log(members[1]); // k8805
+
+// 객체는 중괄호
+var roles = {
+    'programmer' : 'egoing',
+    'designer' : 'k8805',
+    'manager' : 'foo'
+}
+console.log(roles.designer); // k8805
